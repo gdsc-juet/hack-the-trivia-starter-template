81bc6d588ebd2233c72b393949c37bfa9e91adfd
Satyam Kumar <isatyamks@gmail.com>
2024-01-28 10:10:25 +0530

    Add files via upload

---
+++ Technology & Software Dev/Team Sapiens/login/login.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting
+
+        // Get the values entered by the user
+        var username = document.getElementById('username').value.trim();
+        var password = document.getElementById('password').value.trim();
+
+
+        // Check if username and password match
+        if (username === "231b297" && password === "123a") {
+            
+            window.location.href = "../index.html";
+        } else {
+            // Show error message or perform other actions
+            alert("Invalid username or password. Please try again.");
+        }
+    });
+});
